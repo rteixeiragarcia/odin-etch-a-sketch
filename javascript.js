dab4ddceadc2423ebf8b75047e0f1a642cafb719
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-14 08:54:32 -0300

    Create function to setup the canvas with squares

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
+const DEFAULT_SIZE = 16;
+
 const inputRange = document.getElementById("slider");
 const textRange = document.getElementById("size-canvas");
+const canvas = document.getElementById("canvas");
 
-function changeValueCanvas() {
+function changeCanvas() {
     textRange.textContent = `${inputRange.value} x ${inputRange.value}`;
 }
 
-inputRange.addEventListener("change", changeValueCanvas);
+inputRange.addEventListener("change", changeCanvas);
+
+function createCanvas(size) {
+    let widthSquare = canvas.offsetWidth / size;
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        canvas.appendChild(square);
+        square.style.cssText = `width: ${widthSquare}px; height: ${widthSquare}px; border: 1px solid black`;
+    }
+}
