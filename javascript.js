a312a83f99f26b440cd4193a81554b4ee2439fa0
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-15 14:12:58 -0300

    Declare DOM elements

--- javascript.js
+++ javascript.js
@@ -2,3 +2,13 @@ const DEFAULT_SIZE = 16;
 const DEFAULT_COLOR = "rgb(0, 0, 0)";
 const DEFAULT_MODE = "color";
 const DEFAULT_GRAY_SLICE = 25.5;
+
+let canvas = document.getElementById("canvas");
+let sizeCanvas = document.getElementById("size-canvas");
+let slider = document.getElementById("slider");
+let eraserBtn = document.getElementById("eraser-btn");
+let clearBtn = document.getElementById("clear-btn");
+let colorBtn = document.getElementById("color-btn");
+let colorSelector = document.getElementById("color-selector");
+let rgbBtn = document.getElementById("rgb-btn");
+let grayBtn = document.getElementById("gray-btn");
