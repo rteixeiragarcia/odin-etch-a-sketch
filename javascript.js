dea47ecf487be418e6425bcb4722eb9a48f90f85
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-15 14:32:12 -0300

    Declare variables with default values

--- javascript.js
+++ javascript.js
@@ -3,6 +3,11 @@ const DEFAULT_COLOR = "rgb(0, 0, 0)";
 const DEFAULT_MODE = "color";
 const DEFAULT_GRAY_SLICE = 25.5;
 
+let mode = DEFAULT_MODE;
+let size = DEFAULT_SIZE;
+let graySlice = DEFAULT_GRAY_SLICE;
+let color = DEFAULT_COLOR;
+
 let canvas = document.getElementById("canvas");
 let sizeCanvas = document.getElementById("size-canvas");
 let slider = document.getElementById("slider");
@@ -16,3 +21,4 @@ let grayBtn = document.getElementById("gray-btn");
 function setValueInput() {
     this.setAttribute('value',this.value);
 }
+
